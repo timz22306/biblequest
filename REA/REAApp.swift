8458a115b72f3daa9e76ee2a60a01e668f87bc9c
Tim Zhao <tzhaonj@gmail.com>
2025-04-05 12:33:05 -0400

    Initial Commit

---
+++ REA/REAApp.swift
@@ -0,0 +1,17 @@
+//
+//  REAApp.swift
+//  REA
+//
+//  Created by Tim Zhao on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct REAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
